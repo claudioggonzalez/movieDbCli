3ed13675f2ee4223732b765e857747c19278e28c
Claudio Gonzalez <claudioggonzalez@gmail.com>
2021-11-30 22:15:11 -0300

    Agegando primer submenu

--- movieDbCli.py
+++ movieDbCli.py
@@ -1,23 +1,45 @@
 #!/bin/python3
 
+from typing import Optional
 from simple_term_menu import TerminalMenu
 import configparser
 import json
 import requests
-from os import system as exec
 
-def main(address):
-    exec('clear')
-    print (f"Address to connect: {address}\n")
-    print ("OPTIONS:")
-    print ("--------")
-    options = ["Get Movie by ID","Get Movie by Title","Get Batch of Movies","Add Movie","Update Movie Data","Delete Movie", "API Info", "Exit"]
-    menu = TerminalMenu(options)
-    menuEntryIdx = menu.show()
-    exec('clear')
-    print(f"Has elegido la opcion: {menuEntryIdx} - {options[menuEntryIdx]}\n")
+DEBUG = 0
+INFO = 1
+WARNING = 2
+ERROR = 3
+CRITICAL = 4
+
+def printLog(msgLogLevel, logMessage):
+    if msgLogLevel >= LOG_LEVEL:
+        print(logMessage)
+
+def getMovieById():
+    options = ["[1]Define ID to get","[0]Return MAIN MENU"]
+    menuEntryIdx = displayMenu(options, "GET MOVIE BY ID:\n-----------------")
+    
     if options[menuEntryIdx] == options[0]:
-        pass
+        id = int(input("Movie ID: "))
+        requests
+    elif options[menuEntryIdx] == options[1]:
+        main()
+    else:
+        print("Opción Desconocida!!!")
+        exit(1)
+
+def displayMenu(options, menuTitle):
+    menu = TerminalMenu(options, title=menuTitle)
+    idx = menu.show()
+    return idx
+
+def main():
+    options = ["[1]Get Movie by ID","[2]Get Movie by Title","[3]Get Batch of Movies","[4]Add Movie","[5]Update Movie Data","[6]Delete Movie", "[7]API Info", "[0]Exit"]
+    menuEntryIdx = displayMenu(options, "MAIN MENU:\n---------")
+    
+    if options[menuEntryIdx] == options[0]:
+        getMovieById()
     elif options[menuEntryIdx] == options[1]:
         pass
     elif options[menuEntryIdx] == options[2]:
@@ -31,7 +53,7 @@ def main(address):
     elif options[menuEntryIdx] == options[6]:
         pass
     elif options[menuEntryIdx] == options[7]:
-        print("Saliendo...")
+        print("Saliendo...\n")
         exit(0)
     else:
         print("Opción Desconocida!!!")
@@ -43,8 +65,14 @@ if __name__ == "__main__":
         config.read("movieDbCli.conf")
     except:
         print ("ERROR opening config file...!!!")
+        exit(1)
     else:
         address = config["Conection"]["ADDRESS"]
         port = config["Conection"]["PORT"]
+        global LOG_LEVEL
+        LOG_LEVEL = int(config["Logging"]["LOGLEVEL"])
+
+        global fullAddress
         fullAddress = address+":"+port+"/api/"
-        main(fullAddress)
+        printLog(DEBUG, "Address to connect: " + fullAddress + "\n")
+        main()
